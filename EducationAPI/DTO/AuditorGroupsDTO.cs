d4d5248d6804db2cf419cdb0695db6721c76597e
SMFarid <60547678+SMFarid@users.noreply.github.com>
2024-06-19 12:08:25 +0300

    Changed DoneFlag to int instead of Boolean Added States for RoundCode

--- EducationAPI/DTO/AuditorGroupsDTO.cs
+++ EducationAPI/DTO/AuditorGroupsDTO.cs
@@ -3,6 +3,6 @@
     public class AuditorGroupsDTO
     {
         public string RoundCode {  get; set; }
-        public Boolean Doneflag {  get; set; }    
+        public int Doneflag {  get; set; }    
     }
 }
---
+++ EducationAPI/Enums/RoundCodeStates.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationAPI.Enums
+{
+    public enum RoundCodeStates
+    {
+        Open = 0,
+        ReOpened = 1,
+        Done = 2,
+        Abandoned = 9
+    }
+}
